a4b4b234e53bee7283c3fc62238cab31dc76d286
Filip Sadowski <fsadowski81@gmail.com>
2026-05-24 15:45:05 +0200

    tydzien 3 useState

--- tydzien-3-4-react/src/App.jsx
+++ tydzien-3-4-react/src/App.jsx
@@ -2,15 +2,16 @@ import Header from "./components/Header";
 import Pogoda from "./components/Pogoda";
 import Zadania from "./components/Zadania";
 import Footer from "./components/Footer";
+import Licznik from "./components/Licznik";
 
 export default function App() {
-  const moje_zadania = ["Powtórzyć HTML", "Zrobić zakupy", "Pobiegać"];
   return (
     <>
       <Header tytul="Mój panel" />
       <main>
         <Pogoda />
-        <Zadania lista={moje_zadania} />
+        <Licznik />
+        <Zadania lista={[]} />
       </main>
       <Footer autor="Filip Sadowski" />
     </>
--- tydzien-3-4-react/src/components/Zadania.jsx
+++ tydzien-3-4-react/src/components/Zadania.jsx
@@ -1,4 +1,7 @@
-export default function Zadania({ lista }) {
+import { useState } from "react";
+
+export default function Zadania() {
+  const [lista, setLista] = useState(["Powtórzyć HTML", "Zrobić zakupy", "Pobiegać"]);
   return (
     <section>
       <h2>Zadania</h2>
